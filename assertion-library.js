6731842021b59172181f4b5bc5298c3fe2c57b87
opauloh <paulo.scape@gmail.com>
2020-08-28 22:37:44 -0300

    encapsulate our testing functions

--- assertion-library.js
+++ assertion-library.js
@@ -1,11 +1,21 @@
-const sum = (a, b) => a - b;
+const sum = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 
-let result, expected
+function sumTest() {
+  const result = sum(3, 7)
+  const expected = 10;
+  expect(result).toBe(expected)
+}
 
-result = sum(3, 7)
-expected = 10;
-expect(result).toBe(expected)
+function subtractTest() {
+  const result = subtract(7, 3)
+  const expected = 4;
+  expect(result).toBe(expected)
+}
+
+test('sum adds numbers', sumTest)
+test('subtract subtracts numbers', subtractTest)
+
 
 function test(title, callback) {
   try {
