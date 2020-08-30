12170dbde1adf6ebe9be779731a2da8158c9312c
opauloh <paulo.scape@gmail.com>
2020-08-30 11:42:41 -0300

    refactor old code

---
+++ src/fundamentals/setup-globals.js
@@ -0,0 +1,26 @@
+/*
+async function test(title, callback) {
+  try {
+    await callback()
+    console.log(`√ ${title}`)
+  } catch (error) {
+    console.log(`x ${title}`)
+    console.error(error)
+  }
+}
+
+function expect(actual) {
+  return {
+    toBe(expected) {
+      if (actual !== expected) {
+        throw new Error(`${actual} is not equal to ${expected}`)
+      }
+    },
+    toEqual(expected) {},
+    toBeGreaterThan(expected) {},
+  }
+}
+
+global.test = test
+global.expect = expect
+*/
